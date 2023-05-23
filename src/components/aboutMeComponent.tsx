58698941761c40288f1098f97973677ac4d5fb52
wivulo <walterivulo@gmail.com>
2023-05-24 00:56:29 +0200

    circular text completed

--- src/components/aboutMeComponent.tsx
+++ src/components/aboutMeComponent.tsx
@@ -4,23 +4,21 @@ const AboutMeComponent = () => {
 
     return (
         <section id="About" className="flex flex-col justify-center items-center relative h-auto lg:h-screen bg-white z-20">
-            <div className="flex flex-col-reverse lg:flex-row justify-center gap-y-16 lg:gap-y-0 py-10 pb-20 px-10 items-center">
+            <div className="flex flex-col-reverse lg:flex-row justify-center gap-y-16 lg:gap-y-0 py-16 pb-20 px-10 items-center">
                 <div className="flex justify-center items-center relative lg:w-1/2">
                     <Image src="/about-img.62b47e7f183d4b4e9feb.webp" width={500} height={500} alt="about me"
                         className="rounded-md w-80 h-72 lg:w-96 lg:h-96"></Image>
 
                     <div className="grid place-content-center absolute w-44 h-44 rounded-full bg-white z-10 -right-20 -bottom-16 lg:right-10 shadow shadow-black">
                         <div className="flex justify-center items-center h-44 w-44 rounded-full border-2 border-black relative">
-                            <svg width="176" height="176" viewBox="0 0 176 176">
-                                <path id="circle" d="M 88 176 a 88 88 0 1 1 0 -176 a 88 88 0 1 1 0 176" fill="none" stroke="lightgray" stroke-width="2" />
-                                <text font-family="Arial, sans-serif" font-size="10" font-weight="bold">
-                                    <textPath xlinkHref="#circle" startOffset="0%">
-                                        DESENVOLVEDOR FRONT-END REACT DESENVOLVEDOR FRONT-END REACT
+                            <svg viewBox="0 0 500 500" className="rotate">
+                                <path id="curve" fill="white" d="M73.2,148.6c4-6.1,65.5-96.8,178.6-95.6c111.3,1.2,170.8,90.3,175.1,97" />
+                                <text width="500" fontSize={32}>
+                                    <textPath xlinkHref="#curve">
+                                        Desenvolvedor front-end
                                     </textPath>
                                 </text>
                             </svg>
-
-
                         </div>
                     </div>
                 </div>
